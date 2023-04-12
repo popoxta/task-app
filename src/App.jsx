cb8ec01583e0fd7da63b0acdcf1dbe79e5af3bcc
popoxta <popoxta@gmail.com>
2023-04-12 12:08:36 +1200

    add task render from fake tasks array

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const fakeTasks = [
+        {id: 0, name: 'taskName'},
+        {id: 1, name: 'taskName2'},
+        {id: 3, name: 'taskName3'},
+    ]
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function TasksList(){
+        return(
+            fakeTasks.map(task => <li key={task.id}>{task.name}</li>)
+        )
+    }
+
+    return (
+        <div>
+            <h1>TASKS</h1>
+            <TasksList />
+        </div>
+    )
 }
-
-export default App
