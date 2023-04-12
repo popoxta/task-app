eb3aaa471fd6af116b3b6f0af13d351760d105a9
popoxta <popoxta@gmail.com>
2023-04-12 18:29:45 +1200

    refactor ternary to if/else and remove unused arg

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -18,28 +18,24 @@ export default function TasksList({tasks, handleEdit, handleDelete}) {
 function Task({task, handleEdit, handleDelete}) {
     const [isEditing, setIsEditing] = useState(false)
 
-    return (
-        isEditing
-            ?
-            <div className={'task-item'}>
-                <li><input
-                    value={task.name}
-                    onChange={e => handleEdit({...task, name: e.target.value})}
-                /></li>
-
-                <div>
-                    <button onClick={e => setIsEditing(false)}>save</button>
-                    <button onClick={handleDelete}>x</button>
-                </div>
+    if (isEditing) {
+        return <div className={'task-item'}>
+            <li><input
+                value={task.name}
+                onChange={e => handleEdit({...task, name: e.target.value})}
+            /></li>
+            <div>
+                <button onClick={() => setIsEditing(false)}>save</button>
+                <button onClick={handleDelete}>x</button>
             </div>
-            :
-            <div className={'task-item'}>
-                <li>{task.name}</li>
-                <div>
-                    <button onClick={() => setIsEditing(true)}>edit</button>
-                    <button onClick={handleDelete}>x</button>
-                </div>
+        </div>
+    } else {
+        return <div className={'task-item'}>
+            <li>{task.name}</li>
+            <div>
+                <button onClick={() => setIsEditing(true)}>edit</button>
+                <button onClick={handleDelete}>x</button>
             </div>
-
-    )
+        </div>
+    }
 }
