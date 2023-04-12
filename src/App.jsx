c3286385edc2647813ca0c8450085d152377b86b
popoxta <popoxta@gmail.com>
2023-04-12 12:23:16 +1200

    inserted input field for adding tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
         {id: 1, name: 'taskName2'},
         {id: 3, name: 'taskName3'},
     ]
-
     const [tasks, setTasks] = useState(initialTasks)
+    const [text, setText] = useState('')
 
     function TasksList() {
         return (
@@ -15,16 +15,28 @@ export default function App() {
         )
     }
 
-    function addTask() {
+    function addTask(name) {
         console.log(tasks)
         const newId = tasks[tasks.length - 1].id + 1 // create new id for new task
-        setTasks([...tasks, {id: newId, name: `newTask`}])
+        setTasks([...tasks, {id: newId, name}])
     }
 
     return (
         <div>
             <h1>TASKS</h1>
-            <button onClick={addTask}>ADD</button>
+
+            <input type={'text'}
+                   placeholder={'Add task...'}
+                   onChange={e => setText(e.target.value)}/>
+
+            <button type={'submit'}
+                    onClick={e => {
+                        e.preventDefault()
+                        text ? addTask(text) : null
+                    }}>
+                ADD
+            </button>
+
             <TasksList/>
         </div>
     )
