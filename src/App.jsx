69b409a5b5d22d05f87484565e01a1c815950be7
popoxta <popoxta@gmail.com>
2023-04-12 12:14:23 +1200

    put tasks in state and added add task btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import React, {useState} from 'react'
 
 export default function App() {
-    const fakeTasks = [
+    const initialTasks = [
         {id: 0, name: 'taskName'},
         {id: 1, name: 'taskName2'},
         {id: 3, name: 'taskName3'},
     ]
 
-    function TasksList(){
-        return(
-            fakeTasks.map(task => <li key={task.id}>{task.name}</li>)
+    const [tasks, setTasks] = useState(initialTasks)
+
+    function TasksList() {
+        return (
+            tasks.map(task => <li key={task.id}>{task.name}</li>)
         )
+    }
+
+    function addTask() {
+        console.log(tasks)
+        const newId = tasks[tasks.length - 1].id + 1 // create new id for new task
+        setTasks([...tasks, {id: newId, name: `newTask`}])
     }
 
     return (
         <div>
             <h1>TASKS</h1>
-            <TasksList />
+            <button onClick={addTask}>ADD</button>
+            <TasksList/>
         </div>
     )
 }
